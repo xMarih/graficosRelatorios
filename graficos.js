68d383c161f04f23531039e762c1adbd2084ae71
Mariana Eliza Alves Costa <68155516+xMarih@users.noreply.github.com>
2021-05-10 21:03:06 -0300

    mudança de biblioteca

--- graficos.js
+++ graficos.js
@@ -1,33 +1,25 @@
-function intercale(array1, array2) {
-  var arrayResult = [];
-  var total = 0;
-  if (array1.length > array2.length) {
-    total = array1.length;
-  } else {
-    total = array2.length;
-  }
+// function intercale(array1, array2) {
+//   var arrayResult = [];
+//   var total = 0;
+//   if (array1.length > array2.length) {
+//     total = array1.length;
+//   } else {
+//     total = array2.length;
+//   }
 
-  for (var i = 0; i < total; i++) {
-    if (array1[i]) {
-      arrayResult.push(array1[i]);
-    }
-    if (array2[i]) {
-      arrayResult.push(array2[i]);
-    }
-  }
+//   for (var i = 0; i < total; i++) {
+//     if (array1[i]) {
+//       arrayResult.push(array1[i]);
+//     }
+//     if (array2[i]) {
+//       arrayResult.push(array2[i]);
+//     }
+//   }
 
-  return arrayResult;
-}
-
-
+//   return arrayResult;
+// }
 
 var urlBase = 'https://registrocivilminas.org.br/relatorio/';
-
-
-
-
-
-
 
 // let url2 = urlBase + 'obito/tres-anos-covid-quantidade-BH';
 // let xhr = new XMLHttpRequest();
@@ -56,7 +48,6 @@ var urlBase = 'https://registrocivilminas.org.br/relatorio/';
 
 // Graficos
 
-
 function drawChart() {
   let url = urlBase + 'obito/tres-anos-covid-MG';
   let xhr = new XMLHttpRequest();
@@ -68,55 +59,94 @@ function drawChart() {
     if (xhr.readyState == 4) {
       if (xhr.status == 200) {
         var request = xhr.response;
-       
+
         var totalRegistros = request[0]["totalRegistros"];
+
+        var vetMes = new Array(totalRegistros);
+        for (i = 0; i < totalRegistros; i++) {
+          vetMes[i] = request[i]["mes"]
+        }
+
+        var vetquantidadeDeRegistro = new Array(totalRegistros);
+        for (i = 0; i < totalRegistros; i++) {
+          vetquantidadeDeRegistro[i] = parseFloat((request[i]["quantidadeDeRegistro"]).replace('.', ''))
+        }
+        // console.log(intercale(vetMes, vetquantidadeDeRegistro));
         
-        var vetMes = new Array (totalRegistros); 
-        for(i = 0; i < totalRegistros; i++) {
-          vetMes [i] = request[i]["mes"]
-        }   
-        
-        var vetquantidadeDeRegistro = new Array (totalRegistros); 
-        for(i = 0; i < totalRegistros; i++) {
-          vetquantidadeDeRegistro [i] = parseFloat((request[i]["quantidadeDeRegistro"]).replace('.' , ''))
-        }   
-        console.log(intercale(vetMes, vetquantidadeDeRegistro));
+
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var chart = new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: vetMes,
+            datasets: [{
+              label: '# of Votes',
+              data: vetquantidadeDeRegistro,
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              y: {
+                beginAtZero: true
+              }
+            }
+          }
+        });
+       
 
 
 
-        
+
         // var data = google.visualization.arrayToDataTable(
-        //   [vetMes, vetquantidadeDeRegistro]
-        //   ); 
+
+        //   intercale(vetMes, vetquantidadeDeRegistro)
+        // );
 
 
-        var data = google.visualization.arrayToDataTable([
-          ['Mês', 'Mortes Gerais'],
-          [ request[14]["mes"] ,       parseFloat((request[14]["quantidadeDeRegistro"]).replace('.' , ''))],
-          [ request[13]["mes"] ,       parseFloat((request[13]["quantidadeDeRegistro"]).replace('.' , ''))],
-          [ request[12]["mes"] ,       parseFloat((request[12]["quantidadeDeRegistro"]).replace('.' , ''))],
-          [ request[11]["mes"] ,       parseFloat((request[11]["quantidadeDeRegistro"]).replace('.' , ''))],
-          [ request[10]["mes"] ,       parseFloat((request[10]["quantidadeDeRegistro"]).replace('.' , ''))],
-          [ request[9]["mes"] ,       parseFloat((request[9]["quantidadeDeRegistro"]).replace('.' , ''))],
-          [ request[8]["mes"] ,       parseFloat((request[8]["quantidadeDeRegistro"]).replace('.' , ''))],
-          [ request[7]["mes"] ,       parseFloat((request[7]["quantidadeDeRegistro"]).replace('.' , ''))],
-          [ request[6]["mes"] ,       parseFloat((request[6]["quantidadeDeRegistro"]).replace('.' , ''))],
-          [ request[5]["mes"] ,       parseFloat((request[5]["quantidadeDeRegistro"]).replace('.' , ''))],
-          [ request[4]["mes"] ,       parseFloat((request[4]["quantidadeDeRegistro"]).replace('.' , ''))],
-          [ request[3]["mes"] ,       parseFloat((request[3]["quantidadeDeRegistro"]).replace('.' , ''))],
-          [ request[2]["mes"] ,       parseFloat((request[2]["quantidadeDeRegistro"]).replace('.' , ''))],
-          [ request[1]["mes"] ,       parseFloat((request[1]["quantidadeDeRegistro"]).replace('.' , ''))],
+        // var data = google.visualization.arrayToDataTable([
+        //   ['Mês', 'Mortes Gerais'],
+        //   [ request[14]["mes"] ,       parseFloat((request[14]["quantidadeDeRegistro"]).replace('.' , ''))],
+        //   [ request[13]["mes"] ,       parseFloat((request[13]["quantidadeDeRegistro"]).replace('.' , ''))],
+        //   [ request[12]["mes"] ,       parseFloat((request[12]["quantidadeDeRegistro"]).replace('.' , ''))],
+        //   [ request[11]["mes"] ,       parseFloat((request[11]["quantidadeDeRegistro"]).replace('.' , ''))],
+        //   [ request[10]["mes"] ,       parseFloat((request[10]["quantidadeDeRegistro"]).replace('.' , ''))],
+        //   [ request[9]["mes"] ,       parseFloat((request[9]["quantidadeDeRegistro"]).replace('.' , ''))],
+        //   [ request[8]["mes"] ,       parseFloat((request[8]["quantidadeDeRegistro"]).replace('.' , ''))],
+        //   [ request[7]["mes"] ,       parseFloat((request[7]["quantidadeDeRegistro"]).replace('.' , ''))],
+        //   [ request[6]["mes"] ,       parseFloat((request[6]["quantidadeDeRegistro"]).replace('.' , ''))],
+        //   [ request[5]["mes"] ,       parseFloat((request[5]["quantidadeDeRegistro"]).replace('.' , ''))],
+        //   [ request[4]["mes"] ,       parseFloat((request[4]["quantidadeDeRegistro"]).replace('.' , ''))],
+        //   [ request[3]["mes"] ,       parseFloat((request[3]["quantidadeDeRegistro"]).replace('.' , ''))],
+        //   [ request[2]["mes"] ,       parseFloat((request[2]["quantidadeDeRegistro"]).replace('.' , ''))],
+        //   [ request[1]["mes"] ,       parseFloat((request[1]["quantidadeDeRegistro"]).replace('.' , ''))],
 
-        ]);
-      
-        var options = {
-          title: 'Comparativo de Mortes em Minas Gerais',
-          hAxis: { title: 'Ano', titleTextStyle: { color: '#333' } },
-          vAxis: { minValue: 0 }
-        };
-      
-        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
+        // ]);
+
+        // var options = {
+        //   title: 'Comparativo de Mortes em Minas Gerais',
+        //   hAxis: { title: 'Ano', titleTextStyle: { color: '#333' } },
+        //   vAxis: { minValue: 0 }
+        // };
+
+        // var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+        // chart.draw(data, options);
 
 
       } else {
